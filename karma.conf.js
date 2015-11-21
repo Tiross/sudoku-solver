7c43cd76b8fecc5b33599aabadde33ff6a914498
Tiross <Tiross@users.noreply.github.com>
2015-11-21 15:55:37 +0100

    Migration de la conf de Karma dans un fichier
    A voir du coup si ça marche toujours pour Travis

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine'],
+    plugins: [
+      'karma-jasmine',
+      'karma-safari-launcher',
+      'karma-firefox-launcher',
+      'karma-phantomjs-launcher',
+    ],
+    files: [
+      'src/*.js',
+      'spec/*.js',
+    ],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: [
+      'PhantomJS',
+      'Safari',
+      'Firefox',
+    ],
+    singleRun: false,
+    concurrency: Infinity,
+  });
+};
