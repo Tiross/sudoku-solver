6f7972a608f388d99cf98516e7ed92492d03ac28
Tiross <Tiross@users.noreply.github.com>
2015-11-22 16:43:42 +0100

    Simplification des tests

--- spec/sudoku.js
+++ spec/sudoku.js
@@ -1,26 +1,32 @@
 
+var rand = function () {
+  return Math.floor(Math.random() * 9);
+};
+
 describe('Sudoku', function () {
-  it('should be reset with init()', function () {
-    var sudoku = new Sudoku;
+  describe('start behavior', function () {
+    it('should be reset with init()', function () {
+      var sudoku = new Sudoku;
 
-    expect(sudoku.init()).toEqual(new Sudoku);
-  });
+      expect(sudoku.init()).toEqual(new Sudoku);
+    });
 
-  it('should have null value if none inserted', function () {
-    var sudoku = new Sudoku;
+    it('should have null value if none inserted', function () {
+      var sudoku = new Sudoku;
 
-    var line   = Math.floor(Math.random() * 9);
-    var column = Math.floor(Math.random() * 9);
+      var line   = rand();
+      var column = rand();
 
-    expect(sudoku.getValue(line, column)).toBeNull();
-  });
+      expect(sudoku.getValue(line, column)).toBeNull();
+    });
 
-  it('should have a full list of candidates at start', function () {
-    var sudoku = new Sudoku;
+    it('should have a full list of candidates at start', function () {
+      var sudoku = new Sudoku;
 
-    var line   = Math.floor(Math.random() * 9);
-    var column = Math.floor(Math.random() * 9);
+      var line   = rand();
+      var column = rand();
 
-    expect(sudoku.getCandidates(line, column)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      expect(sudoku.getCandidates(line, column)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    });
   });
 });
