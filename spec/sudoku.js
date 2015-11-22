1c1ab67c7a87aaeabd8ea25e2a9f2d462db00bac
Tiross <Tiross@users.noreply.github.com>
2015-11-22 22:03:18 +0100

    Simplification de la gestion des grilles pour les tests

--- spec/sudoku.js
+++ spec/sudoku.js
@@ -3,6 +3,20 @@ var rand = function () {
   return Math.floor(Math.random() * 9);
 };
 
+// jscs:disable
+var oneMissingGrid = [
+  0, 5, 9, 7, 1, 6, 3, 8, 2,
+  6, 1, 2, 3, 8, 9, 7, 4, 5,
+  8, 7, 3, 2, 4, 5, 1, 6, 9,
+  3, 8, 7, 9, 6, 4, 5, 2, 1,
+  5, 2, 4, 1, 7, 3, 6, 9, 8,
+  1, 9, 6, 8, 5, 2, 4, 3, 7,
+  9, 6, 5, 4, 2, 1, 8, 7, 3,
+  7, 3, 1, 6, 9, 8, 2, 5, 4,
+  2, 4, 8, 5, 3, 7, 9, 1, 6,
+];
+// jscs:enable
+
 describe('Sudoku', function () {
   describe('start behavior', function () {
     it('should be reset with init()', function () {
@@ -91,21 +105,7 @@ describe('Sudoku', function () {
     });
 
     it('will mark sudoku as finish', function () {
-      // jscs:disable
-      var grid = [
-        0, 5, 9, 7, 1, 6, 3, 8, 2,
-        6, 1, 2, 3, 8, 9, 7, 4, 5,
-        8, 7, 3, 2, 4, 5, 1, 6, 9,
-        3, 8, 7, 9, 6, 4, 5, 2, 1,
-        5, 2, 4, 1, 7, 3, 6, 9, 8,
-        1, 9, 6, 8, 5, 2, 4, 3, 7,
-        9, 6, 5, 4, 2, 1, 8, 7, 3,
-        7, 3, 1, 6, 9, 8, 2, 5, 4,
-        2, 4, 8, 5, 3, 7, 9, 1, 6,
-      ];
-      // jscs:enable
-
-      var sudoku = new Sudoku(grid);
+      var sudoku = new Sudoku(oneMissingGrid);
 
       expect(sudoku.isFinished()).toBe(false);
       expect(sudoku.addValue(4, 0, 0).isFinished()).toBe(true);
@@ -114,21 +114,7 @@ describe('Sudoku', function () {
 
   describe('resolutions, ', function () {
     describe('findLonelyCandidate', function () {
-      // jscs:disable
-      var grid = [
-        0, 5, 9, 7, 1, 6, 3, 8, 2,
-        6, 1, 2, 3, 8, 9, 7, 4, 5,
-        8, 7, 3, 2, 4, 5, 1, 6, 9,
-        3, 8, 7, 9, 6, 4, 5, 2, 1,
-        5, 2, 4, 1, 7, 3, 6, 9, 8,
-        1, 9, 6, 8, 5, 2, 4, 3, 7,
-        9, 6, 5, 4, 2, 1, 8, 7, 3,
-        7, 3, 1, 6, 9, 8, 2, 5, 4,
-        2, 4, 8, 5, 3, 7, 9, 1, 6,
-      ];
-      // jscs:enable
-
-      var sudoku = new Sudoku(grid);
+      var sudoku = new Sudoku(oneMissingGrid);
 
       it('should return how much values was founded', function () {
         expect(sudoku.findLonelyCandidate()).toBe(1);
